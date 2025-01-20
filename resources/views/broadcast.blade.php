6521b998c34d3f183f08a1b63919421d77056c4f
David Herrera <david.gmzherrera28@gmail.com>
2025-01-19 20:12:14 -0600

    first commit

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+  <p>{{$message}}</p>
+  <img src="https://media.tenor.com/olr0tUuWI7gAAAAj/cuh-cat.gif" alt="Profile picture">
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+  <img src="https://i.pinimg.com/236x/83/a5/04/83a504e9ce8edf1f841cc918fa01e204.jpg" alt="Avatar">
+  <p>{{$message}}</p>
+</div>
